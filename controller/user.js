2391b8ebf46c2e8af54b0420245f258a088ca202
Ayushii40 <ayusonwane@gmail.com>
2026-04-02 12:27:12 +0530

    fix-cors-issue

--- controller/user.js
+++ controller/user.js
@@ -9,7 +9,9 @@ export const sendToken = (user, statusCode, message, res) => {
     expires: new Date(
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
-    httpOnly: true, // Set httpOnly to true for security
+    httpOnly: true,
+    secure: true,
+    sameSite: "None",
   };
 
   res.status(statusCode).cookie("token", token, options).json({
@@ -63,6 +65,8 @@ export const logout = catchAsyncErrors(async (req, res, next) => {
     .cookie("token", "", {
       expires: new Date(Date.now()),
       httpOnly: true,
+      secure: true,
+      sameSite: "None",
     })
     .json({
       success: true,
